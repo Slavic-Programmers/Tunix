d4195a5bc8510e7dcf1cf871637d537a67a7210f
Gavriel Linoy <kraserultra@gmail.com>
2024-05-30 22:56:55 +0300

    Change some stuff in the GUI

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+"""
+    ----------------------------
+    | ~~~ Appearance Modes ~~~ |
+    ----------------------------
+"""
+
+SYSTEM_MODE: str = 'System'
+LIGHT_MODE: str = 'Light'
+DARK_MODE: str = 'Dark'
--- src/tunix.py
+++ src/tunix.py
@@ -1,14 +1,17 @@
 import tkinter
 import customtkinter
+from settings import *
 
 class Tunix:
     def __init__(self) -> None:
         self.__root = tkinter.Tk()
         self.__root.geometry('400x400')
         self.__root.title('Tunix')
+        customtkinter.set_appearance_mode(SYSTEM_MODE)
         
-        button = customtkinter.CTkButton(master=self.__root, corner_radius=10)
-        button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        
+        
+        
 
     def run(self) -> None:
         self.__root.mainloop()
