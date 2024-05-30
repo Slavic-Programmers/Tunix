e5505f20b7e87b91744cefd43cec289ab523ae0e
Gavriel Linoy <kraserultra@gmail.com>
2024-05-30 22:27:28 +0300

    Create music player class + sample mp3

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from tunix import Tunix
+from tunix_music_player import TunixMusicPlayer
 
 def main() -> None:
     tunix = Tunix()
+    tmp = TunixMusicPlayer()
+    tmp.load('./music/song1.mp3')
+    tmp.play()
+    
 
 if __name__ == "__main__":
     main()
